79e84b0fd65081039163443f9500d7becd21d4db
Neurochrom <pcichock@poczta.onet.pl>
2014-01-24 22:22:06 +0100

    We've got a moving green background - yeah.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-alert("hello");
-alert("motherfucker");
-alert("yes you!");
+enchant();
+
+Sprite.prototype.setPosV = function(vec_) {
+    this.x = vec_.x;
+    this.y = vec_.y;
+};
+Sprite.prototype.addToPosV = function(vec_) {
+    this.x += vec_.x;
+    this.y += vec_.y;
+};
+
+window.onload = function(){
+    var game = new Core(960, 640);
+    game.fps = 60;
+    game.preload("img/background.png");
+    game.onload = function(){
+        var background = new Sprite(960,640);
+        background.image = game.assets["img/background.png"];
+        background.x = 0;
+        background.y = 0;
+        game.rootScene.addChild(background);
+        background.addEventListener("enterframe", function(){
+            this.x += 0.1;
+            this.y += 0.1;
+        });
+    };
+    game.start();
+};
