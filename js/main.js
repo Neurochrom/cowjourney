e84d3cd8979e3552b70c4d8b905e4a89b140044f
Aleksander Nitecki <ixendr@itogi.re>
2014-01-25 16:47:49 +0100

    Move world.* from main to this.* in world.

--- js/main.js
+++ js/main.js
@@ -16,53 +16,6 @@ window.onload = function(){
 
     world.init(game);
 
-    world.smell = function() {
-        // O(n^2) - slow as shit quick solution
-        for(var i=0; i<world.animals.length; ++i) {
-            var a1 = world.animals[i];
-            for (var j=0; j<world.animals.length; ++j) {
-                if (j==i)
-                    continue;
-                var a2 = world.animals[j];
-                var to = a2.center().subV(a1.center());
-                if (to.lengthSqr() < a2.rSense*a2.rSense) {
-                    a2.smell(a1);
-                }
-            }
-        }
-    }
-
-    world.findCollidingPairs = function() {
-        // O(n^2) - slow as shit quick solution
-        for(var i=0; i<world.animals.length; ++i) {
-            var a1 = world.animals[i];
-            for (var j=0; j<world.animals.length; ++j) {
-                if (j==i)
-                    continue;
-                var a2 = world.animals[j];
-                var to = a2.center().subV(a1.center());
-                var rSum = a1.rCol + a2.rCol;
-                var colliding = [];
-                if (to.lengthSqr() < rSum*rSum) {
-                    colliding.push([a1, a2]);
-                }
-            }
-        }
-
-        return colliding;
-    };
-
-    world.resolveCollisions = function(colliding) {
-        for (var i=0; i<colliding.length; ++i) {
-            var a1 = colliding[i][0];
-            var a2 = colliding[i][1];
-            if(a1.type != "player_cow")
-                a1.followedObject = null;
-            if(a2.type != "player_cow")
-                a2.followedObject = null;
-        }
-    };
-
     var par = Particle();
     world.start();
  }
--- js/world.js
+++ js/world.js
@@ -94,6 +94,54 @@ var world = (function(){
 
     this.par = null;
     this.music = null;
+
+    this.smell = function() {
+        // O(n^2) - slow as shit quick solution
+        for(var i=0; i<this.animals.length; ++i) {
+            var a1 = this.animals[i];
+            for (var j=0; j<this.animals.length; ++j) {
+                if (j==i)
+                    continue;
+                var a2 = this.animals[j];
+                var to = a2.center().subV(a1.center());
+                if (to.lengthSqr() < a2.rSense*a2.rSense) {
+                    a2.smell(a1);
+                }
+            }
+        }
+    }
+
+    this.findCollidingPairs = function() {
+        // O(n^2) - slow as shit quick solution
+        for(var i=0; i<this.animals.length; ++i) {
+            var a1 = this.animals[i];
+            for (var j=0; j<this.animals.length; ++j) {
+                if (j==i)
+                    continue;
+                var a2 = this.animals[j];
+                var to = a2.center().subV(a1.center());
+                var rSum = a1.rCol + a2.rCol;
+                var colliding = [];
+                if (to.lengthSqr() < rSum*rSum) {
+                    colliding.push([a1, a2]);
+                }
+            }
+        }
+
+        return colliding;
+    };
+
+    this.resolveCollisions = function(colliding) {
+        for (var i=0; i<colliding.length; ++i) {
+            var a1 = colliding[i][0];
+            var a2 = colliding[i][1];
+            if(a1.type != "player_cow")
+                a1.followedObject = null;
+            if(a2.type != "player_cow")
+                a2.followedObject = null;
+        }
+    };
+
     this.start = function(){
         this.par = new Particle();
         this.music = new Music();
