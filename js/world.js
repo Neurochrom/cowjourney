9c45ca5fd912c41b39549b3ab61633ea879c08b7
Aleksander Nitecki <ixendr@itogi.re>
2014-01-25 17:35:46 +0100

    Move definition of colliding array BEFORE loop. Also more whitespace.

--- js/world.js
+++ js/world.js
@@ -120,6 +120,8 @@ var world = (function(){
     }
 
     this.findCollidingPairs = function() {
+        var colliding = [];
+        
         // O(n^2) - slow as shit quick solution
         for(var i=0; i<this.animals.length; ++i) {
 
@@ -129,9 +131,10 @@ var world = (function(){
                     continue;
 
                 var a2 = this.animals[j];
+
                 var to = a2.center().subV(a1.center());
                 var rSum = a1.rCol + a2.rCol;
-                var colliding = [];
+
                 if (to.lengthSqr() < rSum*rSum) {
                     colliding.push([a1, a2]);
                 }
@@ -143,10 +146,13 @@ var world = (function(){
 
     this.resolveCollisions = function(colliding) {
         for (var i=0; i<colliding.length; ++i) {
+
             var a1 = colliding[i][0];
             var a2 = colliding[i][1];
+
             if(a1.type != "player_cow")
                 a1.followedObject = null;
+
             if(a2.type != "player_cow")
                 a2.followedObject = null;
         }
@@ -155,6 +161,7 @@ var world = (function(){
     this.start = function(){
         this.par   = new Particle();
         this.music = new Music();
+
         this.game.preload(this.par.preload);
         this.game.preload(this.music.preload);
         this.game.preload("img/cow_atlas.png");
