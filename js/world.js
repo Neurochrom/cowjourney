ed0a46b3425410d64667cecc49b589673d9e488c
pmusiol <gpiotr@gmail.com>
2014-01-25 15:46:10 +0100

    isObstacleAt : cow don't want to hit the wall

--- js/world.js
+++ js/world.js
@@ -33,16 +33,23 @@ var world = (function(){
     };
     this.size = new Vec2(getWidth(), getHeight());
     this.animals = [];
-    this.obstacles = [];
+    this.obstacles = {};
     this.addAnimal = function(animal){
         this.animals.push(animal);
         this.game.rootScene.addChild(animal);
     };
+    this.isObstacleAt = function(pos){
+        var p = pos.divV(this.field_size);
+        if(this.obstacles[Math.floor(p.y)] && this.obstacles[Math.floor(p.y)][Math.floor(p.x)])
+                return true;
+        return false;
+    };
+    this.field_size = null;
     this.readMap = function(map){
         var maps_size = new Vec2(map[0].length, map.length);
-        var field_size = this.size.divV(maps_size);
+        this.field_size = this.size.divV(maps_size);
         for(var i = 0; i < map.length; i++){
-            this.obstacles[i] = [];
+            this.obstacles[i] = {};
             for(var j = 0; j < map[i].length; j++){
                 var path;
                 switch(map[i][j]){
@@ -66,10 +73,13 @@ var world = (function(){
                 var img = world.game.assets['img/' + path];
                 var sprite = new Sprite(img.width, img.height);
                 sprite.image = img;
-                var paddedField = field_size.mulS(1 + padding * 2);
-               sprite.scale(paddedField.x / img.width, paddedField.y / img.height);
+                var paddedField = this.field_size.mulS(1 + padding * 2);
+                sprite.scale(paddedField.x / img.width, paddedField.y / img.height);
 
-                sprite.pos = field_size.mulV(new Vec2(j * (1 - padding),i * (1 - padding)));
+                sprite.pos = this.field_size.mulV(new Vec2(j * (1 - padding),i * (1 - padding)));
+                this.obstacles[i][j] = sprite;
+
+
                 this.game.rootScene.addChild(sprite);
             }
         }
