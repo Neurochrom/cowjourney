9aa81feb4ed6452fc9546ef66327eb7ef0e57e31
pmusiol <gpiotr@gmail.com>
2014-01-25 14:07:29 +0100

    cool background

--- js/world.js
+++ js/world.js
@@ -77,14 +77,14 @@ var world = (function(){
         this.par = new Particle();
         this.game.preload(this.par.preload);
         this.game.preload("img/cow_atlas.png");
+        this.game.preload("img/grass2.png");
 
         world.game.onload = function(){
             // temp adding of animals here
-            var a1 = new Animal(64, 64, world.game.assets["img/cow_atlas.png"], new Vec2(30, 30), 60, 320);
-            a1.speed = new Vec2(1, 1);
 
             // A background for touch event handling
-            var background = new Entity();
+            var background = new Sprite(world.size.x, world.size.y);
+            background.image = world.game.assets["img/grass2.png"];
             background.addEventListener('touchstart', function(e) {
                 world.par.blood(new Vec2(e.x-12, e.y-12));
                 a1.followedObject = { center: function() { return new Vec2(e.x, e.y); } };
@@ -95,6 +95,8 @@ var world = (function(){
             background.width = world.size.x;
             background.height = world.size.y;
             world.game.rootScene.addChild(background);
+            var a1 = new Animal(64, 64, world.game.assets["img/cow_atlas.png"], new Vec2(30, 30), 60, 320);
+            a1.speed = new Vec2(1, 1);
 
                 world.readMap([
                     [0,1,0,0],
