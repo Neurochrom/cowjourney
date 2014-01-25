c717f5ccd9adcf0b9d5c59f9815a1d2bda2b834c
Neurochrom <pcichock@poczta.onet.pl>
2014-01-25 18:42:42 +0100

    PseudoPhysical cow collisions.

--- js/animal.js
+++ js/animal.js
@@ -24,14 +24,14 @@ var Animal = Class.create(Sprite, {
                 this.speed = this.speed.mulS(0);
             }
             this.pos = this.pos.addV(this.speed);
-            this.speed = this.speed.mulS(this.speed.lengthSqr() > this.rCol ? 0.6 : 0.90);
+            this.speed = this.speed.mulS(this.speed.lengthSqr() > this.rCol ? 0.6 : 0.94);
             if (this.followedObject) {
                 if (this.stunned > 0)
                    this.stunned--;
                 else {
                 var to = this.followedObject.center().subV(this.center());
                 to.normalize();
-                this.speed = this.speed.addV(to.mulS(0.12));
+                this.speed = this.speed.addV(to.mulS(0.14));
                 //this.speed.normalize();
                 }
             } else {
@@ -64,6 +64,20 @@ var pseudoPhysicalCol = function(a1, a2) {
         a2.followedObject = null;
         a2.stunned = 60;
     }
+
+    var to = a1.center().subV(a2.center());
+    var n = new Vec2(to.x, to.y);
+    n.normalize();
+
+    var m1 = a1.rCol*a1.rCol;
+    var m2 = a2.rCol*a2.rCol;
+
+    var pen = to.length() - a1.rCol - a2.rCol;
+    pen = 50 + pen*pen;
+
+    var j = n.mulS((m1+m2)*pen*-0.005);
+    a1.speed = a1.speed.subV(j.divS(m1*1.3));
+    a2.speed = a2.speed.addV(j.divS(m2*1.3));
 };
 
 var Player = Class.create(Animal, {
